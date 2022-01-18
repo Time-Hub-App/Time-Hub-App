4319eab59f88e07dc3a30214acbaa2189c33cae9
ckaynatividad <natividad.ckay@gmail.com>
2022-01-18 11:04:26 -0800

    prettier and eslint and authform

---
+++ src/Components/AuthForm/AuthForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function AuthForm({
+  email,
+  setEmail,
+  username,
+  setUsername,
+  password,
+  setPassword,
+  handleSubmit,
+  errorMsg,
+}) {
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div>
+        <label>Email:</label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <p>{errorMsg}</p>
+      <input type="submit" />
+    </form>
+  );
+}
